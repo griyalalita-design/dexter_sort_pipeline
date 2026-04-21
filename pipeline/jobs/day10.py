689004198eeb7a61ed5b25c815b2a66969bbbdfb
griyalalita-design <griya.lalita@ninjavan.co>
2026-04-21 14:52:13 +0700

    Update day10.py

--- pipeline/jobs/day10.py
+++ pipeline/jobs/day10.py
@@ -1,10 +1,38 @@
-# jobs/day10.py — TODO: implement
-from utils.transform import get_last_month_range
+# jobs/day10.py
+from utils.gsheet import read_sheet, write_sheet
+from config.settings import GSHEET
+
 
 def run():
-    _, _, bulan = get_last_month_range()
-    print(f"=== DAY 10 - bulan {bulan} ===")
-    # TODO: implement
+    print("===== Kita Mulai Run Day 10 CPP =====")
+
+    df_cpp = read_sheet(
+        GSHEET["cpp"]["sheet_id"],
+        GSHEET["cpp"]["tabs"]["main"]
+    )
+    print("===== Ambil Data CPP Done =====")
+    print(f"CPP shape: {df_cpp.shape}")
+
+    print("===== Input ke Tracker =====")
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker"]["sheet_id"],
+        sheet_name=GSHEET["tracker"]["tabs"]["raw_data_cost"],
+        df=df_cpp,
+        start_cell="A3",
+        include_header=False
+    )
+    print("===== Input Data ke Tracker Done =====")
+
+    print("===== Input CPP ke Sanggahan =====")
+    write_sheet(
+        spreadsheet_id=GSHEET["sanggahan"]["sheet_id"],
+        sheet_name=GSHEET["sanggahan"]["tabs"]["cpp"],
+        df=df_cpp,
+        start_cell="A3",
+        include_header=False
+    )
+    print("===== Input CPP ke Sanggahan Done =====")
+
 
 if __name__ == "__main__":
     run()
