292c6bfb42a82aafcae7f02144e7f57502a10297
griyalalita-design <griya.lalita@ninjavan.co>
2026-04-21 13:28:42 +0700

    Implement Day 6 data processing in day6.py
    Implement data reading and writing for Day 6 tasks.

--- pipeline/jobs/day6.py
+++ pipeline/jobs/day6.py
@@ -1,10 +1,38 @@
-# jobs/day6.py — TODO: implement
-from utils.transform import get_last_month_range
+# jobs/day6.py
+from utils.gsheet import read_sheet, write_sheet
+from config.settings import GSHEET
+
 
 def run():
-    _, _, bulan = get_last_month_range()
-    print(f"=== DAY 6 - bulan {bulan} ===")
-    # TODO: implement
+    print("====== Kita Kerjain Day 6 Buat DWS ya =========")
+
+    df_dws = read_sheet(
+        GSHEET["dws"]["sheet_id"],
+        GSHEET["dws"]["tabs"]["main"]
+    )
+
+    print("===== Ambil data dari DWS done =====")
+
+    print("===== Mulai input ke Tracker dulu ya =====")
+    write_sheet(
+        spreadsheet_id=GSHEET["tracker"]["sheet_id"],
+        sheet_name=GSHEET["tracker"]["tabs"]["raw_data_all"],
+        df=df_dws,
+        start_cell="BO4",
+        include_header=False
+    )
+    print("===== Done Input ke Tracker =====")
+
+    print("===== Mulai Input ke Sanggahan ya =====")
+    write_sheet(
+        spreadsheet_id=GSHEET["sanggahan"]["sheet_id"],
+        sheet_name=GSHEET["sanggahan"]["tabs"]["dws"],  # pastikan key ini ada di settings.py
+        df=df_dws,
+        start_cell="A3",
+        include_header=False
+    )
+    print("===== Done Input ke Sanggahan =====")
+
 
 if __name__ == "__main__":
     run()
