13b8d0ac58724531ecee8581a72370107e2e7bdf
griyalalita-design <griya.lalita@ninjavan.co>
2026-04-22 10:39:53 +0700

    Update day15.py

--- pipeline/jobs/day15.py
+++ pipeline/jobs/day15.py
@@ -1,10 +1,9 @@
 # jobs/day15.py — TODO: implement
 from utils.transform import get_last_month_range
-
+from utils.gsheet import read_sheet, write_sheet
+from config.settings import GSHEET
 def run():
-    _, _, bulan = get_last_month_range()
-    print(f"=== DAY 15 - bulan {bulan} ===")
-    # TODO: implement
+    
 
 if __name__ == "__main__":
     run()
