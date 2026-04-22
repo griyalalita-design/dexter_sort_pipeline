626366d6652f7f16a92cae2f5c7eac735cdb5ff0
griyalalita-design <griya.lalita@ninjavan.co>
2026-04-22 13:18:57 +0700

    Add functionality to copy data between Google Sheets
    Implemented data copying from tracker and staff list to converter sheets.

--- pipeline/jobs/day16.py
+++ pipeline/jobs/day16.py
@@ -1,10 +1,24 @@
 # jobs/day16.py — TODO: implement
 from utils.transform import get_last_month_range
+from utils.gsheet import copy_range
+from config.settings import GSHEET
 
 def run():
-    _, _, bulan = get_last_month_range()
-    print(f"=== DAY 16 - bulan {bulan} ===")
-    # TODO: implement
-
+    copy_range(
+        GSHEET["tracker"]["sheet_id"],
+        GSHEET["tracker"]["tabs"]["master_tracker_by_hub"],
+        source_range= "A7:Q",
+        GSHEET["converter"]["sheet_id"],
+        GSHEET["converter"]["tabs"]["master_tracker_by_hub"],
+        dest_start_cell="A7"
+    )
+    copy_range(
+        GSHEET["staff_list"]["sheet_id"],
+        GSHEET["staff_list"]["tabs"]["main"],
+        source_range= "A2:E",
+        GSHEET["converter"]["sheet_id"],
+        GSHEET["converter"]["tabs"]["staff_list"],
+        dest_start_cell="B2"
+    )
 if __name__ == "__main__":
     run()
