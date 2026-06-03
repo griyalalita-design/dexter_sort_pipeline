ae32716b8a1ba262c0fa45ea0cc3d8ae0bef0c58
griyalalita-design <griya.lalita@ninjavan.co>
2026-06-03 15:40:35 +0700

    Update day_mom.py

--- pipeline/jobs/day_mom.py
+++ pipeline/jobs/day_mom.py
@@ -12,30 +12,11 @@ def run():
   )
   snapshot_month = "May-26"
   df_flatten = flatten_master_tracker(
-    df_master_tracker,
+    df_master,
     snapshot_month
   )
   print(df_flatten.shape)
   print(df_flatten.head())
-
-    # write_sheet(
-    #     spreadsheet_id=GSHEET["tracker"]["sheet_id"],
-    #     sheet_name=GSHEET["tracker"]["tabs"]["raw_data_cost"],
-    #     df=df_cpp,
-    #     start_cell="B3",
-    #     include_header=False
-    # )
-    # print("===== Input Data ke Tracker Done =====")
-
-    # print("===== Input CPP ke Sanggahan =====")
-    # write_sheet(
-    #     spreadsheet_id=GSHEET["sanggahan"]["sheet_id"],
-    #     sheet_name=GSHEET["sanggahan"]["tabs"]["cpp"],
-    #     df=df_cpp,
-    #     start_cell="A3",
-    #     include_header=False
-    # )
-    # print("===== Input CPP ke Sanggahan Done =====")
 
 
 if __name__ == "__main__":
