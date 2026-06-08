bdac0393c19410559dcf6e1d14ad362a92c41975
griyalalita-design <griya.lalita@ninjavan.co>
2026-06-08 13:30:28 +0700

    Update day_mom.py

--- pipeline/jobs/day_mom.py
+++ pipeline/jobs/day_mom.py
@@ -1,22 +1,29 @@
-# jobs/day10.py 
-from utils.gsheet import read_sheet, write_sheet, flatten_master_tracker
+from utils.gsheet import read_sheet_allow_duplicate_headers, flatten_master_tracker
 from config.settings import GSHEET
 
 
 def run():
-  print("MoM flatten data mulai.....")
+    print("MoM flatten data mulai.....")
 
-  df_master = read_sheet(
-    GSHEET["tracker"]["sheet_id"], 
-    GSHEET["tracker"]["tabs"]["master_tracker_by_hub"]
-  )
-  snapshot_month = "May-26"
-  df_flatten = flatten_master_tracker(
-    df_master,
-    snapshot_month
-  )
-  print(df_flatten.shape)
-  print(df_flatten.head())
+    df_master = read_sheet_allow_duplicate_headers(
+        GSHEET["tracker"]["sheet_id"],
+        GSHEET["tracker"]["tabs"]["master_tracker_by_hub"]
+    )
+
+    print("Master tracker shape:", df_master.shape)
+    print("Columns:", df_master.columns.tolist())
+
+    snapshot_month = "May-26"
+
+    df_flatten = flatten_master_tracker(
+        df_master,
+        snapshot_month
+    )
+
+    print("Flatten shape:", df_flatten.shape)
+    print(df_flatten.head())
+
+    return df_flatten
 
 
 if __name__ == "__main__":
