262eec733b31d2f9c89655b93cbd94de26e010ab
griyalalita-design <griya.lalita@ninjavan.co>
2026-07-08 13:25:46 +0700

    Update day2.py

--- pipeline/jobs/day2.py
+++ pipeline/jobs/day2.py
@@ -11,7 +11,7 @@ from config.settings import METABASE_CONFIG, GSHEET
 # =========================
 # RUN CONTROL
 # =========================
-RUN_POA = False
+RUN_POA = True
 RUN_LND = True
 
 DUMP_TRACKER = True
